6f6ceae2fb34869af325e94cb3498d4bd4d46c46
billjoynf <45283458+billjoynf@users.noreply.github.com>
2020-01-21 01:07:19 +0200

    init

---
+++ src/main/java/com/example/gitmetrix/GitMetrixApplication.java
@@ -0,0 +1,12 @@
+package com.example.gitmetrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMetrixApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitMetrixApplication.class, args);
+  }
+}
