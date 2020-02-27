5ad1832d3c2f2f20487c9c9e041d08ca6cc60b66
billjoynf <45283458+billjoynf@users.noreply.github.com>
2020-02-27 17:38:47 +0200

    move frontend folder on the top

---
+++ src/main/resources/templates/build/precache-manifest.cae2ce1a5400acb69fafd0ee3c7ee46e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20669bedfbe53399e958762d3843365b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06678ec752115a4a4a01",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3a17016136fdafed1d30",
+    "url": "/static/js/2.0259825c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0259825c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06678ec752115a4a4a01",
+    "url": "/static/js/main.8acc11b0.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
